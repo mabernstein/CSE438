31b197927690d98cfba1b6c4b5cbee871a8dd17f
mabernstein <31412233+mabernstein@users.noreply.github.com>
2018-09-24 11:29:03 -0500

    Add files via upload
    Lab 2 commit 1

---
+++ Lab2/Lab2-uiViewStack/Lab2-uiViewStack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab2-uiViewStack
+//
+//  Created by Max Bernstein on 9/13/18.
+//  Copyright © 2018 Max Bernstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lab2/MaxBernstein-Lab2(3)/MaxBernstein-Lab2(3)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaxBernstein-Lab2(3)
+//
+//  Created by Max Bernstein on 9/19/18.
+//  Copyright © 2018 Max Bernstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
